ea6356beba92c53f181a062d92de4777fe6868e9
Clayton <clayton.almeida.campos@gmail.com>
2021-07-05 11:17:35 -0300

    test map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -20,26 +20,20 @@ const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
 const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
 const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID
 
-//verifica se tem Mapbox se nao tiver ele pega o openstreetmap
-// const CustomTileLayer = () => {
-//   //   return MAPBOX_API_KEY ? (
-//   //     <TileLayer
-//   //       attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//   //       url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}.html?fresh=true&title=view&access_token=${MAPBOX_API_KEY}`}
-//   //     />
-//   //   ) : (
-//   //     <TileLayer
-//   //       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//   //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//   //     />
-//   //   )
-//   // }
-
-//   ;<TileLayer
-//     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//   />
-// }
+// verifica se tem Mapbox se nao tiver ele pega o openstreetmap
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/512/0/0/0@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+  )
+}
 
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
@@ -50,10 +44,7 @@ const Map = ({ places }: MapProps) => {
       zoom={3}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer
-        attribution='© <a href="https://apps.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}.html?fresh=true&title=view&access_token=${MAPBOX_API_KEY}`}
-      />
+      <CustomTileLayer />
       {places?.map(({ id, name, location, slug }) => {
         const { latitude, longitude } = location
 
